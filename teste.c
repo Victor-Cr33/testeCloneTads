385abc7a4815a4d9193321a347e2189e6d23f2b3
Caio Lemos <caio.969@outlook.com>
2020-02-14 10:12:07 -0300

    talvez eu saiba verificar par ou impar

--- teste.c
+++ teste.c
@@ -3,6 +3,24 @@
 
 int main(){
 
+    int num; 
+
+   printf("Digite um número: ");
+    
+    scanf("%i", &num);
+
+    if(num % 2 == 0){
+
+       printf("par...");
+    }
+    else{
+       printf("Impar...");
+    }
+
+
+
+ 
+ printf("\n");
 
     return 0;
 }
